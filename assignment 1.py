4e8d073af37484118368fc5a181f6ba2772e05c3
manufccr007 <107296627+manufccr007@users.noreply.github.com>
2024-04-09 15:09:46 +0530

    Pw skills , Data science assignment-1

---
+++ assignment 1.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## question-1
+
+# In[ ]:
+
+
+variable=("Apple", "Orange", 2.5, [5,6,7])
+
+
+# In[5]:
+
+
+type(variable[0])
+
+
+# ## question-2
+
+# In[6]:
+
+
+var1=''
+
+
+# In[7]:
+
+
+type(var1)
+
+
+# In[8]:
+
+
+var2='[DS,ML,Python]'
+
+
+# In[10]:
+
+
+type(var2)
+
+
+# In[11]:
+
+
+var3=['DS','ML','Python']
+
+
+# In[13]:
+
+
+type(var3)
+
+
+# In[14]:
+
+
+var4=1
+
+
+# In[15]:
+
+
+type(var4)
+
+
+# ## question-3
+
+# ## use of following operator.
+
+# In[18]:
+
+
+a=15
+b=5
+
+
+# In[19]:
+
+
+a/b
+
+
+# In[20]:
+
+
+a%b
+
+
+# In[24]:
+
+
+a=5
+b=2
+
+
+# In[25]:
+
+
+a%b
+
+
+# In[30]:
+
+
+a=8
+b=3
+
+
+# In[31]:
+
+
+a//b
+
+
+# In[32]:
+
+
+a=2
+
+
+# In[33]:
+
+
+a**2
+
+
+# ## question-4
+
+# In[37]:
+
+
+list=["apple",1,2,3,20,25.6,"iftikar",4+4j,20,True]
+
+
+# In[42]:
+
+
+for i in list:
+    print(i)
+    print(type(i))
+
+
+# ## question7
+
+# In[43]:
+
+
+list=[2,5,6,8]
+
+
+# In[44]:
+
+
+list[1]=9
+
+
+# In[45]:
+
+
+list
+
+
+# In[48]:
+
+
+tuple=(5,20,30,60)
+
+
+# In[49]:
+
+
+tuple[1]=5
+
+
+# In[ ]:
+
+
+
+
